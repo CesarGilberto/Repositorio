f84d7173f7c8aec242e605d5a6edcfe48199947f
GURBERRASECK <cesarin_gfc@hotamil.es>
2020-04-22 18:28:08 -0600

    RETO

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
-};
+  production: true,
+    firebaseConfig : {
+    apiKey: 'AIzaSyAZvOPFtmjQ0ZvJXJnzNv7_iPk4OhJ_EZ0',
+    authDomain: 'actividad1-7fd84.firebaseapp.com',
+    databaseURL: 'https://actividad1-7fd84.firebaseio.com',
+    projectId: 'actividad1-7fd84',
+    storageBucket: 'actividad1-7fd84.appspot.com',
+    messagingSenderId: '156291205892',
+    appId: '1:156291205892:web:205f8b0242e780dbab9e51'
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,28 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    firebaseConfig : {
+    apiKey: 'AIzaSyAZvOPFtmjQ0ZvJXJnzNv7_iPk4OhJ_EZ0',
+    authDomain: 'actividad1-7fd84.firebaseapp.com',
+    databaseURL: 'https://actividad1-7fd84.firebaseio.com',
+    projectId: 'actividad1-7fd84',
+    storageBucket: 'actividad1-7fd84.appspot.com',
+    messagingSenderId: '156291205892',
+    appId: '1:156291205892:web:205f8b0242e780dbab9e51'
+  }
+};
+export const environment2 = {
+  production: true,
+    firebaseConfig : {
+    apiKey: 'AIzaSyAZvOPFtmjQ0ZvJXJnzNv7_iPk4OhJ_EZ0',
+    authDomain: 'actividad1-7fd84.firebaseapp.com',
+    databaseURL: 'https://actividad1-7fd84.firebaseio.com',
+    projectId: 'actividad1-7fd84',
+    storageBucket: 'actividad1-7fd84.appspot.com',
+    messagingSenderId: '156291205892',
+    appId: '1:156291205892:web:205f8b0242e780dbab9e51'
+  }
 };
 
 /*
